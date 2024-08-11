71c2bacbdd8bf8ef78a3b362f319c2fa30e4052f
Chiki975 <carlosalzamorahuaman@gmail.com>
2024-08-10 22:38:02 -0500

    nuevos cambios y agregando pdfs

--- src/pages/Certificados.jsx
+++ src/pages/Certificados.jsx
@@ -1,21 +1,57 @@
-import excel from "../pdf/excel.pdf";
+import excel from "../pdf/1.pdf";
+import ingles from "../pdf/2.pdf";
+import soporte from "../pdf/2.pdf";
 import "../styles/certificados-style.css";
 
 function Certificados() {
   return (
     <div className="certificados-container">
-      <h2>Certificados</h2>
+      <h3>Certificados</h3>
 
       <div className="certificados">
         <div>
           <h3>Excel</h3>
-          <iframe src={excel}></iframe>
+          <iframe src={`${excel}#toolbar=0&navpanes=0&scrollbar=0`}></iframe>
 
           <h4>Descripción:</h4>
           <p>
-            Certificado conseguido en la
-            <strong>Universidad Texnologica del Peru</strong> en el curso
-            <strong>Innovacion y transformacion digital</strong> del ciclo 10.
+            Certificado obtenido en la{" "}
+            <strong>Universidad Tecnológica del Perú</strong> por haber
+            completado el curso de{" "}
+            <strong>Innovación y Transformación Digital</strong> correspondiente
+            al ciclo 10.
+          </p>
+
+          <button>DESCARGAR</button>
+        </div>
+
+        <div>
+          <h3>Excel</h3>
+          <iframe src={`${excel}#toolbar=0&navpanes=0&scrollbar=0`}></iframe>
+
+          <h4>Descripción:</h4>
+          <p>
+            Certificado obtenido en la{" "}
+            <strong>Universidad Tecnológica del Perú</strong> por haber
+            completado el curso de{" "}
+            <strong>Innovación y Transformación Digital</strong> correspondiente
+            al ciclo 10.
+          </p>
+
+          <button>DESCARGAR</button>
+        </div>
+
+        <div>
+          <h3>Excel</h3>
+          <iframe src={`${excel}#toolbar=0&navpanes=0&scrollbar=0`}></iframe>
+
+          <h4>Descripción:</h4>
+          <p>
+            Certificado obtenido en la{" "}
+            <strong>Universidad Tecnológica del Perú</strong> por haber
+            completado el curso de{" "}
+            <strong>Innovación y Transformación Digital</strong> correspondiente
+            al ciclo 10.
           </p>
 
           <button>DESCARGAR</button>
