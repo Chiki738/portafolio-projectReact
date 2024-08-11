6f3d27ba6020c5bbcc718aa589c8c5c7908e9796
Chiki975 <carlosalzamorahuaman@gmail.com>
2024-08-10 19:11:07 -0500

    nuevos cambios

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,25 +6,8 @@ function Footer() {
       <p>
         <strong>© 2024 Mi Portafolio. Todos los derechos reservados.</strong>
       </p>
+
       <div className="redes-enlace">
-        <a
-          href="https://api.whatsapp.com/send?phone=51956299119"
-          title="WhatsApp"
-          target="_blank"
-          rel="noopener noreferrer">
-          <strong>WhatsApp</strong>&nbsp;
-          <i className="fa-brands fa-whatsapp" id="whatsapp"></i>
-        </a>
-
-        <a
-          href="https://t.me/Chiki738"
-          title="Telegram"
-          target="_blank"
-          rel="noopener noreferrer">
-          <strong>Telegram</strong>&nbsp;
-          <i className="fa-brands fa-telegram" id="telegram"></i>
-        </a>
-
         <a
           href="https://www.linkedin.com/in/daniel738/"
           title="LinkedIn"
@@ -41,6 +24,24 @@ function Footer() {
           rel="noopener noreferrer">
           <strong>GitHub</strong>&nbsp;
           <i className="fa-brands fa-github" id="github"></i>
+        </a>
+
+        <a
+          href="https://api.whatsapp.com/send?phone=51956299119"
+          title="WhatsApp"
+          target="_blank"
+          rel="noopener noreferrer">
+          <strong>WhatsApp</strong>&nbsp;
+          <i className="fa-brands fa-whatsapp" id="whatsapp"></i>
+        </a>
+
+        <a
+          href="https://t.me/Chiki738"
+          title="Telegram"
+          target="_blank"
+          rel="noopener noreferrer">
+          <strong>Telegram</strong>&nbsp;
+          <i className="fa-brands fa-telegram" id="telegram"></i>
         </a>
 
         <a
